9c71d985a8e11fde9478f1ada5d9248bf77b5d7c
J-90s <jniksa2@oakland.edu>
2026-09-22 22:20:08 -0400

    Add buildable Android starter: app/src/test/java/com/example/mobileappstarter/StarterUnitTest.kt

---
+++ app/src/test/java/com/example/mobileappstarter/StarterUnitTest.kt
@@ -0,0 +1,10 @@
+package com.example.mobileappstarter
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class StarterUnitTest {
+    @Test fun templateLoads() {
+        assertTrue(true)
+    }
+}
