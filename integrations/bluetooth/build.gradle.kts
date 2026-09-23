9da1f473939285e3315aff6aba8d050b32341214
J-90s <jniksa2@oakland.edu>
2026-09-23 08:50:22 -0400

    Wire module integrations/bluetooth/build.gradle.kts

---
+++ integrations/bluetooth/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.example.integrations.bluetooth"
+    compileSdk = 36
+    defaultConfig { minSdk = 26 }
+    compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
+    kotlinOptions { jvmTarget = "17" }
+}
