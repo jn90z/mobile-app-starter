937351343e1a7e802568313faf13882735debff5
J-90s <jniksa2@oakland.edu>
2026-09-23 09:13:19 -0400

    Remove deprecated Kotlin jvmTarget DSL

--- integrations/camera/build.gradle.kts
+++ integrations/camera/build.gradle.kts
@@ -7,7 +7,6 @@ android {
     compileSdk = 36
     defaultConfig { minSdk = 26 }
     compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
-    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
